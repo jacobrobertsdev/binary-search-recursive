df3f8474ec0c45c6a186d91ea1a584d429576ffe
Jacob Roberts <jacobroberts.dev@gmail.com>
2024-01-12 09:35:39 -0600

    Update mid variable

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function binarySearch(n, array) {
-  let middle = Math.floor(array.length / 2);
+  let middleIndex = Math.floor(array.length / 2);
 
   if (array.length === 1 && array[0] !== n) {
     return "Number not found in the array";
-  } else if (array[middle] === n) {
+  } else if (array[middleIndex] === n) {
     return `Found your number: ${n}`;
-  } else if (n < array[middle]) {
-    return binarySearch(n, array.slice(0, middle));
-  } else if (n > array[middle]) {
-    return binarySearch(n, array.slice(middle));
+  } else if (n < array[middleIndex]) {
+    return binarySearch(n, array.slice(0, middleIndex));
+  } else if (n > array[middleIndex]) {
+    return binarySearch(n, array.slice(middleIndex));
   }
 }
 
